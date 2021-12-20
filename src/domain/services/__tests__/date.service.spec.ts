640d9add396594378327d3c0232a8d61022d1888
pedrokohler <pedrokohlerbh@gmail.com>
2021-12-20 20:28:04 -0300

    Get weekday of the first day of the month

--- src/domain/services/__tests__/date.service.spec.ts
+++ src/domain/services/__tests__/date.service.spec.ts
@@ -1,11 +1,25 @@
-import { getLastDayOfMonth } from "../date.service"
+import { getLastDayOfMonth, getWeekdayOfFirstDayOfMonth } from "../date.service"
 
 describe("DATE TIME SERVICE", () => {
-  it("Should return the last day of the month", () => {
-    expect(getLastDayOfMonth(2021, 2)).toBe(28)
+  describe("GET LAST DAY OF MONTH", () => {
+    it("Should return the last day of the month", () => {
+      expect(getLastDayOfMonth(2021, 2)).toBe(28)
+    })
+
+    it("Should return the last day of the month of a leap year", () => {
+      expect(getLastDayOfMonth(2020, 2)).toBe(29)
+    })
   })
 
-  it("Should return the last day of the month of a leap year", () => {
-    expect(getLastDayOfMonth(2020, 2)).toBe(29)
+  describe("GET WEEKDAY OF FIRST DAY OF MONTH", () => {
+    it("Should return the weekday of the first day of the month", () => {
+      expect(getWeekdayOfFirstDayOfMonth(2021, 11)).toBe(1); // monday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 6)).toBe(2); // tuesday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 12)).toBe(3); // wednesday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 7)).toBe(4); // thursday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 10)).toBe(5); // friday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 5)).toBe(6); // saturday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 8)).toBe(7); //sunday
+    })
   })
 })
--- src/domain/services/date.service.ts
+++ src/domain/services/date.service.ts
@@ -1,6 +1,11 @@
 import { DateTime } from 'luxon';
 
-export const getLastDayOfMonth = (year: number, month: number) => {
+export const getLastDayOfMonth = (year: number, month: number): number => {
   const dateTime = DateTime.local().set({ year, month });
   return dateTime.endOf('month').day as number;
+}
+
+export const getWeekdayOfFirstDayOfMonth = (year: number, month: number): number => {
+  const dateTime = DateTime.local().set({ year, month });
+  return dateTime.startOf('month').weekday
 }
