54a2bdfa8ecf0ee8053137cec5b007bb404947c0
pedrokohler <pedrokohlerbh@gmail.com>
2021-12-20 20:48:34 -0300

    Add weekday enum

---
+++ src/domain/enums/weekdays.enum.ts
@@ -0,0 +1,9 @@
+export enum Weekdays {
+  MONDAY = 1,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
--- src/domain/services/__tests__/date.service.spec.ts
+++ src/domain/services/__tests__/date.service.spec.ts
@@ -1,3 +1,4 @@
+import { Weekdays } from "../../enums/weekdays.enum"
 import { getLastDayOfMonth, getWeekdayOfFirstDayOfMonth } from "../date.service"
 
 describe("DATE TIME SERVICE", () => {
@@ -13,13 +14,15 @@ describe("DATE TIME SERVICE", () => {
 
   describe("GET WEEKDAY OF FIRST DAY OF MONTH", () => {
     it("Should return the weekday of the first day of the month", () => {
-      expect(getWeekdayOfFirstDayOfMonth(2021, 11)).toBe(1); // monday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 6)).toBe(2); // tuesday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 12)).toBe(3); // wednesday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 7)).toBe(4); // thursday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 10)).toBe(5); // friday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 5)).toBe(6); // saturday
-      expect(getWeekdayOfFirstDayOfMonth(2021, 8)).toBe(7); //sunday
+      expect(getWeekdayOfFirstDayOfMonth(2021, 11)).toBe(Weekdays.MONDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 6)).toBe(Weekdays.TUESDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 12)).toBe(Weekdays.WEDNESDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 7)).toBe(Weekdays.THURSDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 10)).toBe(Weekdays.FRIDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 5)).toBe(Weekdays.SATURDAY);
+      expect(getWeekdayOfFirstDayOfMonth(2021, 8)).toBe(Weekdays.SUNDAY);
+    })
+  })
     })
   })
 })
