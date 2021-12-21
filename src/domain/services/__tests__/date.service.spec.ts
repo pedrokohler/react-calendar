bce03bd887ae566c350c6ed16ae966f91429bc4c
pedrokohler <pedrokohlerbh@gmail.com>
2021-12-20 22:53:18 -0300

    Get month matrix row

--- src/domain/services/__tests__/date.service.spec.ts
+++ src/domain/services/__tests__/date.service.spec.ts
@@ -1,5 +1,5 @@
 import { Weekdays } from "../../enums/weekdays.enum"
-import { getLastDayOfMonth, getNumberOfWeekRows, getWeekdayOfFirstDayOfMonth } from "../date.service"
+import { getLastDayOfMonth, getMonthMatrixRow, getNumberOfMonthMatrixRows, getWeekdayOfFirstDayOfMonth } from "../date.service"
 
 describe("DATE TIME SERVICE", () => {
   describe("GET LAST DAY OF MONTH", () => {
@@ -24,14 +24,63 @@ describe("DATE TIME SERVICE", () => {
     })
   })
 
-  describe("GET NUMBER OF WEEKS ROWS OF MONTH", () => {
-    it("Should get the number of weeks rows when the month should have 5 week rows", () => {
-      expect(getNumberOfWeekRows(2021, 11)).toBe(5);
-      expect(getNumberOfWeekRows(2021, 12)).toBe(5);
+  describe("GET NUMBER OF MONTH MATRIX ROWS OF MONTH", () => {
+    it("Should get the number of month matrix rows when the month should have 6 rows", () => {
+      expect(getNumberOfMonthMatrixRows(2021, 10)).toBe(6);
+      expect(getNumberOfMonthMatrixRows(2019, 6)).toBe(6);
     })
 
-    it("Should get the number of weeks when the month should have 4 week rows", () => {
-      expect(getNumberOfWeekRows(2015, 2)).toBe(4);
+    it("Should get the number of month matrix rows when the month should have 5 rows", () => {
+      expect(getNumberOfMonthMatrixRows(2021, 11)).toBe(5);
+      expect(getNumberOfMonthMatrixRows(2021, 12)).toBe(5);
+    })
+
+    it("Should get the number of month matrix rows when the month should have 4 rows", () => {
+      expect(getNumberOfMonthMatrixRows(2015, 2)).toBe(4);
+    })
   })
+
+  describe("GET MONTH MATRIX ROW", () => {
+    it("Should return the 0th row of the matrix correctly", () => {
+      expect(getMonthMatrixRow(0, 31, 0)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+      expect(getMonthMatrixRow(1, 31,  0)).toEqual([null, 1, 2, 3, 4, 5, 6]);
+      expect(getMonthMatrixRow(6, 31,  0)).toEqual([null, null, null, null, null, null, 1]);
+    });
+
+    it("Should return the nth (n > 0) row of the matrix correctly", () => {
+      expect(getMonthMatrixRow(6, 31, 1)).toEqual([2, 3, 4, 5, 6, 7, 8]);
+      expect(getMonthMatrixRow(6, 31, 2)).toEqual([9, 10, 11, 12, 13, 14, 15]);
+      expect(getMonthMatrixRow(6, 31, 3)).toEqual([16, 17, 18, 19, 20, 21, 22]);
+      expect(getMonthMatrixRow(6, 31, 4)).toEqual([23, 24, 25, 26, 27, 28, 29]);
+      expect(getMonthMatrixRow(6, 31, 5)).toEqual([30, 31, null, null, null, null, null]);
+
+      expect(getMonthMatrixRow(5, 31, 1)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+      expect(getMonthMatrixRow(5, 31, 2)).toEqual([10, 11, 12, 13, 14, 15, 16]);
+      expect(getMonthMatrixRow(5, 31, 3)).toEqual([17, 18, 19, 20, 21, 22, 23]);
+      expect(getMonthMatrixRow(5, 31, 4)).toEqual([24, 25, 26, 27, 28, 29, 30]);
+      expect(getMonthMatrixRow(5, 31, 5)).toEqual([31, null, null, null, null, null, null]);
+
+      expect(getMonthMatrixRow(5, 30, 1)).toEqual([3, 4, 5, 6, 7, 8, 9]);
+      expect(getMonthMatrixRow(5, 30, 2)).toEqual([10, 11, 12, 13, 14, 15, 16]);
+      expect(getMonthMatrixRow(5, 30, 3)).toEqual([17, 18, 19, 20, 21, 22, 23]);
+      expect(getMonthMatrixRow(5, 30, 4)).toEqual([24, 25, 26, 27, 28, 29, 30]);
+
+      expect(getMonthMatrixRow(4, 28, 1)).toEqual([4, 5, 6, 7, 8, 9, 10]);
+      expect(getMonthMatrixRow(4, 28, 2)).toEqual([11, 12, 13, 14, 15, 16, 17]);
+      expect(getMonthMatrixRow(4, 28, 3)).toEqual([18, 19, 20, 21, 22, 23, 24]);
+      expect(getMonthMatrixRow(4, 28, 4)).toEqual([25, 26, 27, 28, null, null, null]);
+    })
   })
+
+  // describe("GET MONTH MATRIX", () => {
+  //   it("Should return a correct month matrix for a given year and month", () => {
+  //     expect(getMonthMatrix(2021, 12)).toEqual([
+  //       [null, null, null, 1, 2, 3, 4],
+  //       [5, 6, 7, 8, 9, 10, 11],
+  //       [12, 13, 14, 15, 16, 17, 18],
+  //       [19, 20, 21, 22, 23, 24, 25],
+  //       [26, 27, 28, 29, 30, 31, null]
+  //     ]);
+  //   })
+  // })
 })
--- src/domain/services/date.service.ts
+++ src/domain/services/date.service.ts
@@ -1,9 +1,7 @@
 import { DateTime } from 'luxon';
 import { Weekdays } from '../enums/weekdays.enum';
 
-const MIN_NUMBER_OF_DAYS_IN_A_MONTH = 28;
-const MAX_NUMBER_OF_WEEK_ROWS = 5;
-const MIN_NUMBER_OF_WEEK_ROWS = 4;
+const DAYS_IN_A_WEEK = 7;
 
 const getDateTimeFromYearAndMonth = (year: number, month: number): DateTime => {
   return DateTime.local().set({ year, month });
@@ -19,18 +17,64 @@ export const getWeekdayOfFirstDayOfMonth = (year: number, month: number): number
   return dateTime.startOf('month').weekday
 }
 
-export const getNumberOfWeekRows = (year: number, month: number) => {
+export const getNumberOfMonthMatrixRows = (year: number, month: number) => {
+  const MIN_NUMBER_OF_DAYS_IN_A_MONTH = 28;
+  const MAX_NUMBER_OF_DAYS_IN_A_MONTH = 31;
+  const MAX_NUMBER_OF_WEEK_ROWS = 6;
+  const MIN_NUMBER_OF_WEEK_ROWS = 4;
+
   const lastDay = getLastDayOfMonth(year, month);
-
-  if(lastDay > MIN_NUMBER_OF_DAYS_IN_A_MONTH) {
-    return MAX_NUMBER_OF_WEEK_ROWS;
-  }
-
   const firstWeekdayOfMonth = getWeekdayOfFirstDayOfMonth(year, month);
 
-  if(firstWeekdayOfMonth === Weekdays.SUNDAY){
-    return MIN_NUMBER_OF_WEEK_ROWS;
+  if(lastDay === MIN_NUMBER_OF_DAYS_IN_A_MONTH) {
+    if(firstWeekdayOfMonth === Weekdays.SUNDAY){
+      return MIN_NUMBER_OF_WEEK_ROWS;
+    }
+    return MIN_NUMBER_OF_WEEK_ROWS + 1;
   }
 
-  return MAX_NUMBER_OF_WEEK_ROWS;
+  if(lastDay === MAX_NUMBER_OF_DAYS_IN_A_MONTH) {
+    if(firstWeekdayOfMonth >= Weekdays.FRIDAY) {
+    return MAX_NUMBER_OF_WEEK_ROWS;
+    }
+    return MAX_NUMBER_OF_WEEK_ROWS - 1;
+  }
+
+  if(lastDay === MAX_NUMBER_OF_DAYS_IN_A_MONTH - 1){
+    if(firstWeekdayOfMonth >= Weekdays.SATURDAY){
+      return MAX_NUMBER_OF_WEEK_ROWS
+    }
+    return MAX_NUMBER_OF_WEEK_ROWS - 1;
+  }
+
+  return MAX_NUMBER_OF_WEEK_ROWS - 1;
+}
+
+const getNullArray = (size: number) => new Array(size).fill(null);
+const getSequenceArray = (size: number, base: number) => new Array(size).fill(null).map((_, i) => i + base);
+
+const get0thMonthMatrixRow = (firstDayOfMonthIndex: number, lastDayOf0thRow: number) => {
+  return [...getNullArray(firstDayOfMonthIndex), ...getSequenceArray(lastDayOf0thRow, 1)]
+}
+
+const isLessThanAWeek = (days: number) => days <= DAYS_IN_A_WEEK;
+
+const getNthMonthMatrixRow = (lastDayOfMonth: number, currentRow: number, lastDayOf0thRow: number) => {
+  const firstDayOfRow = (currentRow - 1) * DAYS_IN_A_WEEK + lastDayOf0thRow + 1;
+  const remainingMonthDays = lastDayOfMonth - firstDayOfRow + 1;
+  const hasLessRemainingDaysThanAWeek = isLessThanAWeek(remainingMonthDays);
+  const filledSpots =  hasLessRemainingDaysThanAWeek ? remainingMonthDays : DAYS_IN_A_WEEK;
+  const nullSpots = hasLessRemainingDaysThanAWeek ? DAYS_IN_A_WEEK - remainingMonthDays : 0;
+
+  return [...getSequenceArray(filledSpots, firstDayOfRow), ...getNullArray(nullSpots)];
+}
+
+export const getMonthMatrixRow = (firstDayOfMonthIndex: number, lastDayOfMonth: number, currentRow: number) => {
+  const lastDayOf0thRow = DAYS_IN_A_WEEK - firstDayOfMonthIndex;
+
+  if(currentRow === 0) {
+    return get0thMonthMatrixRow(firstDayOfMonthIndex, lastDayOf0thRow);
+  }
+
+  return getNthMonthMatrixRow(lastDayOfMonth, currentRow, lastDayOf0thRow);
 }
